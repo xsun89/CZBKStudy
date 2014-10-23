74afa644483a67767a81c3c521271b3a24841b29
xsun89 <sunxin89@gmail.com>
2014-10-23 16:46:53 -0700

    a

---
+++ EnterpriseLinkQueue/linkqueue.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "linkqueue.h"
+
+LinkQueue* LinkQueue_Create()
+{
+
+}
+
+void LinkQueue_Destroy(LinkQueue* queue)
+{
+
+}
+
+void LinkQueue_Clear(LinkQueue* queue)
+{
+
+}
+
+int LinkQueue_Append(LinkQueue* queue, void* item)
+{
+
+}
+
+void* LinkQueue_Retrieve(LinkQueue* queue)
+{
+
+}
+
+void* LinkQueue_Header(LinkQueue* queue)
+{
+
+}
+
+int LinkQueue_Length(LinkQueue* queue)
+{
+
+}
